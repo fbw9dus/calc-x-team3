5bea167e38e0edda7bc12a6d9847deb178341f71
centillion-303 <aldana.abdullah1@gmail.com>
2019-12-16 10:38:31 +0100

    finish

--- modules/aspect-ratio.js
+++ modules/aspect-ratio.js
@@ -1,4 +1,4 @@
-export  var oWidth = document.querySelector('#ratio_1')
+/* export  var oWidth = document.querySelector('#ratio_1')
 export  var oHeight = document.querySelector('#ratio_2')
 
 
@@ -10,22 +10,22 @@ export  var resultHeight = document.querySelector('#ratio_result_height')
 oWidth.addEventListener("keyup", calculateAspectRatio)
 oHeight.addEventListener("keyup", calculateAspectRatio)
 resultWidth.addEventListener("keyup", calculateAspectRatio)
-resultHeight.addEventListener("keyup", calculateAspectRatio)
+resultHeight.addEventListener("keyup", calculateAspectRatio) */
 
 
-export function calculateAspectRatio(originalWidth, originalHeight, newValues) {
-    let originalWidth= parseInt(oWidth.value)
-    let originalHeight = parseInt(oHeight.value)
+export function calculateAspectRatio(originalWidth, originalHeight, newValues, target) {
+   // let originalWidth= parseInt(oWidth.value)
+   // let originalHeight = parseInt(oHeight.value)
 
     let resultWidth = newValues[0];
     let resultHeight = newValues[1];
   
     if ((originalWidth > 0) && (originalHeight> 0)) {
         newValues.forEach(newValue => {
-            if ((newValue.classList.contains("height")) && (newValue.value > 0)) {
+            if ((newValue.classList.contains("height")) && (newValue == target)) {
                 return resultWidth.value = ((originalWidth / originalHeight) * newValue.value).toFixed(2);
                 
-            } else if ((newValue.classList.contains("width")) && (newValue.value > 0)) {
+            } else if ((newValue.classList.contains("width")) && (newValue == target)) {
                 newValue.style.outline = "pink";
                 return resultHeight.value = ((originalHeight / originalWidth) * newValue.value).toFixed(2);
             }
--- modules/index.js
+++ modules/index.js
@@ -12,4 +12,21 @@ percentageOf()
 
 difference()
 
-calculateAspectRatio()
+
+
+
+var ratio_1 = document.querySelector('#ratio_1')
+var ratio_2 = document.querySelector('#ratio_2')
+var ratio_new_height = document.querySelector('#ratio_result_height')
+var ratio_new_width = document.querySelector('#ratio_result_width')
+var newValues = [ratio_new_width,ratio_new_height ]
+
+newValues.forEach(input => {
+    input.addEventListener('keyup', getResult)
+})
+
+function getResult(e) {
+    var origWidth = parseInt(ratio_1.value)
+    var origHeight = parseInt(ratio_2.value)
+    calculateAspectRatio(origWidth, origHeight, newValues, e.target)
+}
